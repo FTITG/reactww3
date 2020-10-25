fde922336c3b53c95d9841239e637423ac19cc11
zhaoyue <2241633500@qq.com>
2020-10-25 16:48:03 +0800

    add

---
+++ precache-manifest.c526183e20c2a9f7fc4c2101f81549d1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cb8914151399735f452238f270bc653",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8369dba76611a761ac1a",
+    "url": "./static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "a767aca427902f2aa190",
+    "url": "./static/css/main.36868886.chunk.css"
+  },
+  {
+    "revision": "8369dba76611a761ac1a",
+    "url": "./static/js/2.372fbc06.chunk.js"
+  },
+  {
+    "revision": "de5fa9cfc3624bea3d7565f048bfaaaf",
+    "url": "./static/js/2.372fbc06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a767aca427902f2aa190",
+    "url": "./static/js/main.37ef8d1f.chunk.js"
+  },
+  {
+    "revision": "705f434324a29d64b932",
+    "url": "./static/js/runtime-main.075efe5b.js"
+  },
+  {
+    "revision": "2c8cb6396036cbc402156743291a0545",
+    "url": "./static/media/banner.2c8cb639.jpg"
+  }
+]);
